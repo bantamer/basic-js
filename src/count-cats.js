316884a3c843d1ef4075e5e334556aa62cab9e85
bantamer <pazhitnykhpetr@gmail.com>
2021-03-28 14:35:44 +0300

    feat: add solutiin countCats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,21 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let result = 0;
+  matrix.forEach(arr => {
+    arr.forEach(el => {
+      if (el === '^^') {
+        result += 1;
+      }
+    });
+  });
+  return result;
 };
+
+// console.log(
+//   countCats([
+//     ['##', 'dd', '00'],
+//     ['^^', '..', 'ss'],
+//     ['AA', 'dd', '^^'],
+//   ])
+// );
