717661294db7039973ce55c2f717692cc837e493
bantamer <pazhitnykhpetr@gmail.com>
2021-03-29 17:44:19 +0300

    feat: add solutions

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,21 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+/* eslint-disable no-plusplus */
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw Error;
+  }
+  const resultArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--discard-next') {
+      i++;
+    } else if (arr[i] === '--discard-prev') {
+      if (arr[i - 2] !== '--discard-next') resultArr.pop();
+    } else if (arr[i] === '--double-next') {
+      if (i < arr.length - 1) resultArr.push(arr[i + 1]);
+    } else if (arr[i] === '--double-prev') {
+      if (i > 0 && arr[i - 2] !== '--discard-next') resultArr.push(arr[i - 1]);
+    } else {
+      resultArr.push(arr[i]);
+    }
+  }
+  return resultArr;
 };
